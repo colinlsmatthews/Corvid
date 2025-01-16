450e749242223a18b08b5a60b7816a2f99596b3c
Colin Matthews <25379751+colinlsmatthews@users.noreply.github.com>
2025-01-16 16:37:16 -0500

    add plugin info

--- Raven/RavenInfo.cs
+++ Raven/RavenInfo.cs
@@ -13,15 +13,15 @@ namespace Raven
         public override Bitmap Icon => null;
 
         //Return a short string describing the purpose of this GHA library.
-        public override string Description => "";
+        public override string Description => "A plugin for managing document user text in Rhino files as well as Rhino license and user information";
 
         public override Guid Id => new Guid("033a8c00-7ab7-43ce-9665-e7d1874b6f85");
 
         //Return a string identifying you or your company.
-        public override string AuthorName => "";
+        public override string AuthorName => "Colin Matthews";
 
         //Return a string representing your preferred contact details.
-        public override string AuthorContact => "";
+        public override string AuthorContact => "support@colinlsmatthews.com";
 
         //Return a string representing the version.  This returns the same version as the assembly.
         public override string AssemblyVersion => GetType().Assembly.GetName().Version.ToString();
