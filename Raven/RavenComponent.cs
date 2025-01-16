2ddc7da7d66a3e94909c064e4dc3900a91b29d5d
Colin Matthews <25379751+colinlsmatthews@users.noreply.github.com>
2025-01-16 16:31:16 -0500

    Initial commit

---
+++ Raven/RavenComponent.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Raven
+{
+    public class RavenComponent : GH_Component
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public RavenComponent()
+          : base("Rhino Version", "Version",
+            "Gets the version of the current Rhino installation.",
+            "Params", "Raven")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Rhino Version", "V", "The version of the current Rhino installation.", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Major Version", "M", "The major version of the current Rhino installation.", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Minor Version", "m", "The minor version of the current Rhino installation.", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Build", "B", "The build number of the current Rhino installation.", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Revision", "R", "The revision number of the current Rhino installation.", GH_ParamAccess.item);
+            pManager.AddGenericParameter("Build Date", "D", "The build date of the current Rhino installation.", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            DA.SetData(0, Rhino.RhinoApp.Version.ToString());
+            DA.SetData(1, Rhino.RhinoApp.Version.Major);
+            DA.SetData(2, Rhino.RhinoApp.Version.Minor);
+            DA.SetData(3, Rhino.RhinoApp.Version.Build);
+            DA.SetData(4, Rhino.RhinoApp.Version.Revision);
+            DA.SetData(5, Rhino.RhinoApp.BuildDate);
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// You can add image files to your project resources and access them like this:
+        /// return Resources.IconForThisComponent;
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => null;
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("290721f9-b0e0-409c-9226-9aca12731c8e");
+    }
+}
---
+++ Raven/RavenInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper;
+using Grasshopper.Kernel;
+
+namespace Raven
+{
+    public class RavenInfo : GH_AssemblyInfo
+    {
+        public override string Name => "Raven";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("033a8c00-7ab7-43ce-9665-e7d1874b6f85");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+
+        //Return a string representing the version.  This returns the same version as the assembly.
+        public override string AssemblyVersion => GetType().Assembly.GetName().Version.ToString();
+    }
+}
