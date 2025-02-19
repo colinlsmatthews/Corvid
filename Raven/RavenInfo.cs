61b3723e5759315d05f6f417150516b4bc749715
Colin Matthews <25379751+colinlsmatthews@users.noreply.github.com>
2025-02-18 22:04:25 -0500

    create logo

--- Raven/RavenInfo.cs
+++ Raven/RavenInfo.cs
@@ -13,12 +13,12 @@ namespace Raven
         public override Bitmap Icon => null;
 
         //Return a short string describing the purpose of this GHA library.
-        public override string Description => "A plugin for managing document user text in Rhino files as well as Rhino license and user information";
+        public override string Description => "A plugin for record-keeping and getting information about Rhino";
 
         public override Guid Id => new Guid("033a8c00-7ab7-43ce-9665-e7d1874b6f85");
 
         //Return a string identifying you or your company.
-        public override string AuthorName => "Colin Matthews";
+        public override string AuthorName => "Colin L.S. Matthews";
 
         //Return a string representing your preferred contact details.
         public override string AuthorContact => "support@colinlsmatthews.com";
